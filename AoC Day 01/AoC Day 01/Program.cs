0e27cf82e198c401637bae47e94dcefca33f6e67
Kevin Borduas <kborduas@gmail.com>
2022-07-01 15:06:05 -0400

    Add project files.

---
+++ AoC Day 01/AoC Day 01/Program.cs
@@ -0,0 +1,42 @@
+﻿using Utilities;
+
+SolvePuzzleOne();
+SolvePuzzleTwo();
+
+void SolvePuzzleOne()
+{
+    var cpt = 0;
+    var myValue = DataLoader.GetStringDataFromFile().First();
+
+    foreach (var c in myValue)
+    {
+        if (c == ')')
+            cpt--;
+        else
+            cpt++;
+    }
+
+    Console.WriteLine($"Réponse 1 : {cpt}");
+}
+
+void SolvePuzzleTwo()
+{
+    var cpt = 0;
+    var index = 0;
+    var myValue = DataLoader.GetStringDataFromFile().First();
+
+    foreach (var c in myValue)
+    {
+        index++;
+
+        if (c == ')')
+            cpt--;
+        else
+            cpt++;
+
+        if (cpt < 0)
+            break;
+    }
+
+    Console.WriteLine($"Réponse 2 : {index}");
+}
